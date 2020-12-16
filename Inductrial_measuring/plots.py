99c9a1a8a5d541e04c431ffd22d89dce910a9c22
margolek <margolek555@gmail.com>
2020-12-16 20:41:45 +0100

    Update
    Add functionality to convert text to column

---
+++ Inductrial_measuring/plots.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+filename = 'data.txt'
+
+with open(filename) as file_obj:
+	content = file_obj.readlines()
+
+
+column1 = [i.replace(' ',',') for i in content]
+column2 = [i.replace('\n','') for i in column1]
+	
+
+print(content)
+
