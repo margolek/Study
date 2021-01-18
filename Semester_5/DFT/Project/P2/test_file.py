f8debbcff4d18d7e08c7118f4a0696e021d609ce
margolek <margolek555@gmail.com>
2021-01-18 16:42:26 +0100

    Update test_file.py

--- Semester_5/DFT/Project/P2/test_file.py
+++ Semester_5/DFT/Project/P2/test_file.py
@@ -2,12 +2,18 @@ import numpy as np
 
 
 my_dict = {'dict0.0':np.array([1,2,3,4]),'dict1.0':np.array([5,6,7,8]),'dict2.0':np.array([4,2,7,9])}
-print(my_dict)
+#print(my_dict)
 arr1 = np.array([[1,0],[3,0]])
 arr2 = np.array([[5,6],[7,7]])
-print(x for x in my_dict.keys())
+#print(x for x in my_dict.keys())
 
 
-for n in np.arange(0.0,3.0,1.0):
-	print(n)
+#for n in np.arange(0.0,3.0,1.0):
+	#print(n)
 #print(np.count_nonzero(arr1))
+x = np.array([1,2,3,4])
+print(x.mean())
+y = np.array([1,1,2,3])
+z = np.sum(x*y)
+print(z)
+print(1/3)
