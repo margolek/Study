6bc3ad063fe8d6dca2a453be3ec3e1289b8b968c
margolek <margolek555@gmail.com>
2021-01-18 09:40:39 +0100

    Update

--- Semester_5/DFT/Project/P2/Zadanie_4.py
+++ Semester_5/DFT/Project/P2/Zadanie_4.py
@@ -30,13 +30,26 @@ def create_blocks(x):
     windowsize_r = 8
     windowsize_c = 8
     window = np.array([])
-    for r in range(0,x.shape[0] - windowsize_r, windowsize_r):
-        for c in range(0,x.shape[1] - windowsize_c, windowsize_c):
+    blocks = []
+    for r in range(0,x.shape[0], windowsize_r):
+        for c in range(0,x.shape[1], windowsize_c):
             window = x[r:r+windowsize_r,c:c+windowsize_c]
-    print(window)
+            window = dct(window,axis=1)
+            blocks.append(window)
+    blocks = np.asarray(blocks)
+    for n in range(0,33):
+        blocks1 = np.concatenate((blocks[0],blocks[1],blocks[2],blocks[3],blocks[4],blocks[5]),axis=1)     
+    plt.figure(1)
+    plt.imshow(blocks1,plt.cm.gray)
+    plt.show()
+    for r in range(0,x.shape[0], windowsize_r):
+        for c in range(0,x.shape[1], windowsize_c):
+            window = x[r:r+windowsize_r,c:c+windowsize_c]
+            window = dct(window,axis=1)
+            window = dct(window,axis=0)
+    plt.figure(2)
     plt.imshow(window,plt.cm.gray)
     plt.show()
-
 if __name__ == "__main__":
     image = read_pgm("lena256.pgm", byteorder='<')
     image = np.array(image)
--- Semester_5/DFT/Project/P2/test_file.py
+++ Semester_5/DFT/Project/P2/test_file.py
@@ -2,10 +2,8 @@ import numpy as np
 
 
 
-arr1 = np.array([[1,2,3,4],[3,2,3,4],[5,4,6,4],[7,6,4,3]])
-print(arr1)
+arr1 = np.array([[1,2],[3,4]])
+arr2 = np.array([[5,6],[7,7]])
 
-D,V = np.linalg.eig(arr1)
-print(D)
-print(V)
-print(V.transpose())
+arr3 = np.concatenate((arr1,arr2),axis=1)
+print(arr3)
