c3699d31d92ac874b1eb573748a940cb503797ab
margolek <margolek555@gmail.com>
2021-02-02 16:00:40 +0100

    Update newfile.py

--- Semester_5/DFT/Kolokwium/newfile.py
+++ Semester_5/DFT/Kolokwium/newfile.py
@@ -7,4 +7,4 @@ import matplotlib.pyplot as plt
 #print()
 #print(np.convolve([1,2,3,4],[1,3,1,2],'full'))
 energia = np.abs(-2+0j)**2+2*np.abs(0+3j)**2+2*np.abs(2)**2+2*np.abs(0+5j)**2+2*np.abs(6)**2
-print(energia)
+print(f'Energia wynosi{energia}')
